3d1e725b801e080e0789c3531164026471cad493
TobyKaufman <kaufman.t@northeastern.edu>
2025-02-07 14:17:17 -0500

    Linter bug fix

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Header = () => {
@@ -6,31 +7,31 @@ const Header = () => {
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo or Name */}
         <div className="text-2xl font-bold">
-          <a href="/" className="hover:text-gray-300 transition-colors">
+          <Link href="/" className="hover:text-gray-300 transition-colors">
             Toby Kaufman
-          </a>
+          </Link>
         </div>
 
         {/* Navigation Links */}
         <nav className="space-x-6">
-          <a
+          <Link
             href="/about"
             className="px-4 py-2 border-2 border-white rounded-lg hover:bg-white hover:text-black transition-colors"
           >
             About
-          </a>
-          <a
+          </Link>
+          <Link
             href="/resume"
             className="px-4 py-2 border-2 border-white rounded-lg hover:bg-white hover:text-black transition-colors"
           >
             Resume
-          </a>
-          <a
+          </Link>
+          <Link
             href="/marketwatch"
             className="px-4 py-2 border-2 border-white rounded-lg hover:bg-white hover:text-black transition-colors"
           >
             Market Watch
-          </a>
+          </Link>
         </nav>
       </div>
     </header>
